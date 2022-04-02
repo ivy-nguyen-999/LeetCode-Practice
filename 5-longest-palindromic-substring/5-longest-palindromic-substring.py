dbbf190eb2c634a59e0ed695a7292f0956958074
Ivy Nguyen <60948830+ivy-nguyen-999@users.noreply.github.com>
2022-04-02 16:05:26 -0700

    Time: 206 ms (96.00%), Space: 14.2 MB (20.55%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,63 @@
+class Solution:
+        
+    # palindrome can be even or odd. Adding # in between letters
+    # in the string make all palindrome become odd palindrome
+    def oddPalindrome(self, s):
+        new = "#"
+        for letter in s:
+            new += letter + "#"
+        return new
+    
+    # remove all # before return the palindrome
+    def palindrome(self, substr):
+        return substr[1::2]
+    
+    # manacher's algorithm
+    def longestPalindrome(self, s: str) -> str:
+        s = self.oddPalindrome(s) # len becomes 2n + 1
+        
+        # initialize an array to contain the length of all palindromes
+        P = [0] * (len(s))
+        
+        # variable to store index of the final center
+        finalCenter = 0
+        maxLen = 0
+        
+        # variables to contain the current center and its right boundary
+        center = 0
+        right = 0
+        
+        # loop while the current center is valid
+        for index in range(len(s)):
+            # find mirror of index 
+            mirror = 2 * center - index
+            
+            # if the index is less than the right boundary
+            if (index < right):
+                P[index] = min(right - index, P[mirror])
+            
+            # find the largest palindrome around this center
+            iRight = index + P[index] + 1
+            iLeft = index - P[index] - 1
+            while(iRight < len(s) and iLeft >= 0 and s[iRight] == s[iLeft]):
+                P[index] += 1
+                iRight += 1
+                iLeft -= 1
+            
+            # if the palindome expands beyond the right boundary of the current
+            # palindrome at the center
+            if(index + P[index] > right):
+                # update new center
+                center = index
+                right = index + P[index]
+                # update the final substring if needed
+                if (P[index] > maxLen):
+                    finalCenter = index
+                    maxLen = P[index]
+        
+        result = s[finalCenter - maxLen: finalCenter + maxLen + 1]
+        return self.palindrome(result)
+        
+    
+
+            
