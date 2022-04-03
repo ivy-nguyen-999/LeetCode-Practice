de5a6ebc075f8f57c6a30ffb7a8f58dc6ba119b3
Ivy Nguyen <60948830+ivy-nguyen-999@users.noreply.github.com>
2022-04-02 17:32:42 -0700

    Time: 124 ms (83.67%), Space: 14.5 MB (91.70%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         if(s == s[::-1]):
             return True
-        
         # have extra letters        
         left = 0
         right = len(s) - 1
@@ -14,7 +13,11 @@ class Solution:
                 s1 = s[:right] + s[right+1:]
                 s2 = s[:left] + s[left+1:]
                 # return true if one of them is palindrome
-                return s1 == s1[::-1] or s2 == s2[::-1]
+                if(s1 == s1[::-1] or s2 == s2[::-1]):
+                    return True
+                else:
+                    # return False if none of them is palindrome
+                    return False
             # update left and right
             left += 1
             right -= 1
