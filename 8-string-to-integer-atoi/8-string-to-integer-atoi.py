95b6c6acea14f42f9947da7d7276d2526d64c795
Ivy Nguyen <60948830+ivy-nguyen-999@users.noreply.github.com>
2022-04-06 16:49:32 -0700

    Time: 45 ms (64.29%), Space: 13.8 MB (83.30%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -19,10 +19,10 @@ class Solution:
             return 0
         else:
             result = int(num)
-            if result >= -2**31 and result <= 2**31 - 1:
+            if result >= -pow(2,31) and result <= pow(2,31) - 1:
                 return result
             elif result < 0:
-                 return -2**31
+                 return -pow(2,31)
             else:
-                return 2**31 - 1
+                return pow(2,31) - 1
                 
