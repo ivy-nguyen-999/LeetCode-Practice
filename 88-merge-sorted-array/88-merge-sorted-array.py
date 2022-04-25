8d314a566c7b120652aa8ea21f5e54304ca9bc30
Ivy Nguyen <60948830+ivy-nguyen-999@users.noreply.github.com>
2022-04-25 16:11:02 -0700

    Time: 42 ms (71.90%), Space: 13.8 MB (87.18%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,22 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
+        # start from the last
         current_index = m + n - 1
         current_nums1_index = m - 1
         current_nums2_index = n - 1
         
-        while(current_index >= 0):
-            is_nums1_exhausted = current_nums1_index < 0
-            is_nums2_exhausted = current_nums2_index < 0
+        # looping
+        while(current_index > -1):
             
-            if(not is_nums1_exhausted) and (is_nums2_exhausted or nums1[current_nums1_index] > nums2[current_nums2_index]):
+            if current_nums1_index >= 0 and (current_nums2_index < 0 or nums1[current_nums1_index] > nums2[current_nums2_index]):
                 nums1[current_index] = nums1[current_nums1_index]
                 current_nums1_index -= 1
             else:
                 nums1[current_index] = nums2[current_nums2_index]
                 current_nums2_index -= 1
             
+            # decrement the index
             current_index -= 1
         
         
