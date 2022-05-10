24e26cb1dac529a769f208ba5c0983839c83ebc4
Ivy Nguyen <60948830+ivy-nguyen-999@users.noreply.github.com>
2022-05-10 16:59:41 -0700

    Time: 605 ms (12.11%), Space: 21.8 MB (24.75%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,23 +3,39 @@ class Solution:
         ROWS, COLUMNS = len(grid), len(grid[0])
         # to record which land already visited
         visit = set()
-        
-        def dfs(row, column, visit):
+        """
+        def dfs(row, column):
             #base cases
             if((row, column) in visit or row < 0 or row == ROWS 
                or column < 0 or column == COLUMNS or grid[row][column] == '0'):
                 return
             visit.add((row, column))
-            dfs(row - 1, column, visit)
-            dfs(row + 1, column, visit)
-            dfs(row, column - 1, visit)
-            dfs(row, column + 1, visit)
-        
+            dfs(row - 1, column)
+            dfs(row + 1, column)
+            dfs(row, column - 1)
+            dfs(row, column + 1)
+        """
+        def bfs(row, column):
+            q = collections.deque()
+            visit.add((row, column))
+            q.append((row, column))
+            
+            while q:
+                row, column = q.popleft()
+                direction = [[0,1], [1,0], [0,-1], [-1,0]]
+                
+                for dr, dc in direction:
+                    r, c = row + dr, column + dc
+                    if (r in range(ROWS) and c in range(COLUMNS) and
+                        grid[r][c] == '1' and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+                
         result = 0
         for row in range(ROWS):
             for column in range(COLUMNS):
                 if (grid[row][column] == '1') and ((row, column) not in visit):
                     result += 1
-                    dfs(row, column, visit)
+                    bfs(row, column)
         
         return result
