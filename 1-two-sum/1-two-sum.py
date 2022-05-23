b76761de0566fb3fa2843746d2ed7980b6c5861c
Ivy Nguyen <60948830+ivy-nguyen-999@users.noreply.github.com>
2022-05-22 17:36:12 -0700

    Time: 108 ms (47.62%), Space: 15.2 MB (49.40%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
         for i in range(len(nums)):
             match = target - nums[i]
             if match in nums[i+1:]:
                 return[i, nums.index(match, i + 1)]
+        '''
+        
+        #create a map to contain all visited elements
+        prevMap = {}
+        for i in range(len(nums)):
+            match = target - nums[i]
+            if match in prevMap:
+                return [prevMap[match], i]
+            prevMap[nums[i]] = i
+            
             
